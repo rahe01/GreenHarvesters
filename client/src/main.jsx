58a3e1e775ce5ab2e0031e6d01119ec3829e8078
Rahe <130391604+rahe01@users.noreply.github.com>
2024-08-04 19:06:57 +0600

    d

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './Routes/Routes.jsx'
+import AuthProvider from './providers/AuthProvider'
+import { router } from './routes/Routes'
+import { Toaster } from 'react-hot-toast'
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <HelmetProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </AuthProvider>
+  </HelmetProvider>
 )
