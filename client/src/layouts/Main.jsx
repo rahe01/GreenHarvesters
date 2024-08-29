93d8cca4f4152b5c25463aa9a94dc7af26177b0b
Rahe <130391604+rahe01@users.noreply.github.com>
2024-08-29 19:58:17 +0600

    footer

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Shared/Navbar/Navbar'
-import Footer from '../components/Shared/Footer/Footer'
+
 import Nav from '../components/Shared/Navbar/Nav'
 const Main = () => {
   return (
@@ -10,7 +10,7 @@ const Main = () => {
       <div className='pt-24 min-h-[calc(100vh-68px)]'>
         <Outlet />
       </div>
-      <Footer />
+      
     </div>
   )
 }
