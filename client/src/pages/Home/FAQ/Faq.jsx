43c38beda91461c80a5e000fcdc224fe8b97c0a5
Rahe <130391604+rahe01@users.noreply.github.com>
2024-09-03 22:28:02 +0600

    d

--- client/src/pages/Home/FAQ/Faq.jsx
+++ client/src/pages/Home/FAQ/Faq.jsx
@@ -18,7 +18,7 @@ const Faq = () => {
    <div className="collapse collapse-arrow bg-base-200">
           <input type="radio" name="my-accordion-2" defaultChecked />
           <div className="collapse-title text-xl font-medium">
-            
+          Click to open this one and close others
           </div>
           <div className="collapse-content">
             <p>hello</p>
