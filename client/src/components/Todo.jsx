3001c9799b1c04db5b6c3bb2d698d6c0e0f312fd
Rahe <130391604+rahe01@users.noreply.github.com>
2024-09-17 22:26:23 +0600

    d

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -37,6 +37,7 @@ const Todo = () => {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-5">
             <h1 className="text-2xl font-bold mb-4">Simple To-Do App</h1>
+            <p>sdfjsdf</p>
 
             {/* Input for adding a new todo */}
             <div className="flex space-x-2 mb-4">
